e14c5ca63a9191a0295b1bac40a0b505f6e570a3
SNEYKERZ <105942062+SNEYKERZ@users.noreply.github.com>
2022-05-20 17:12:16 -0500

    Add files via upload

---
+++ queries.sql
@@ -0,0 +1,25 @@
+-- Consultas --
+/*
+Encuentre el dept, title de los instructores registrados en la base de datos.
+*/
+SELECT dept, title FROM instructor
+
+/*
+Indique el nombre y programa del estudiante con student_id = 7656
+*/
+SELECT name, program FROM student 
+WHERE student_id = 7656
+
+/*
+Encuentre los nombres de todos los estudiantes que se han matriculado en el curso con course_id = 837873
+*/
+SELECT name FROM student 
+	JOIN course
+	ON course_id = 837873
+    
+/*
+Cree una vista llamada better_students que presente los estudiantes que obtuvieron las notas más altas por cada semestre entre los años 1900 y 2018
+*/
+CREATE VIEW better_students AS
+	SELECT student_id FROM enrolls
+	WHERE grade > 3.00 AND year BETWEEN 1900 AND 2018;
